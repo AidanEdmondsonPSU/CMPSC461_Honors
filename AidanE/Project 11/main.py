58d796fa168764d6edebb2de184a5143a8fbc759
AidanEdmondsonPSU <157417227+AidanEdmondsonPSU@users.noreply.github.com>
2024-12-05 20:30:11 -0500

    Add files via upload

---
+++ AidanE/Project 11/main.py
@@ -0,0 +1,10 @@
+def main():
+    text = input("Enter a string: ")
+    vowels = "aeiouAEIOU"
+    count = 0
+    for char in text:
+        if char in vowels:
+            count += 1
+    print("Number of vowels:", count)
+if __name__ == "__main__":
+    main()
