decdf83b2d221246767f0b6fc210ed80635ddda9
Ananya Gollakota <76816318+ananyag12@users.noreply.github.com>
2024-12-05 20:30:12 -0500

    Update main.py

--- AidanE/Project 5/main.py
+++ AidanE/Project 5/main.py
@@ -1,6 +1,10 @@
-secret = 7
-guess = int(input("Guess a number between 1 and 10: "))
-if guess == secret:
-    print("Correct!")
-else:
-    print("Try again.")
+def main():
+    secret = 7
+    guess = int(input("Guess a number between 1 and 10: "))
+    if guess == secret:
+        print("Correct!")
+    else:
+        print("Try again.")
+
+if __name__ == "__main__":
+    main()
