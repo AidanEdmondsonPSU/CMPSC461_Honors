f8315be7e6a6f373a12998558456f4ebf3fe2697
Ananya Gollakota <76816318+ananyag12@users.noreply.github.com>
2024-12-05 20:24:21 -0500

    Update main.py

--- AidanE/Project 9/main.py
+++ AidanE/Project 9/main.py
@@ -1,4 +1,7 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-c = int(input("Enter third number: "))
-print("Maximum number is:", max(a, b, c))
+def main ():#line:1
+    OOO0OO0OOOO0OOOOO =int (input ("Enter first number: "))#line:2
+    O000O00OOO0000O00 =int (input ("Enter second number: "))#line:3
+    O000000000O0OO000 =int (input ("Enter third number: "))#line:4
+    print ("Maximum number is:",max (OOO0OO0OOOO0OOOOO ,O000O00OOO0000O00 ,O000000000O0OO000 ))#line:5
+if __name__ =="__main__":#line:7
+    main ()
