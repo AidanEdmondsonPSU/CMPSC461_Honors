ad3f5e0516d180d0aae0fe6a54beca0a4a5f25a2
Ananya Gollakota <76816318+ananyag12@users.noreply.github.com>
2024-12-05 20:33:40 -0500

    Create main_ob.py

---
+++ AidanE/Project 3/main_ob.py
@@ -0,0 +1,12 @@
+def main ():#line:1
+    for OOO00OO0O00O00000 in range (1 ,16 ):#line:2
+        if OOO00OO0O00O00000 %3 ==0 and OOO00OO0O00O00000 %5 ==0 :#line:3
+            print ("FizzBuzz")#line:4
+        elif OOO00OO0O00O00000 %3 ==0 :#line:5
+            print ("Fizz")#line:6
+        elif OOO00OO0O00O00000 %5 ==0 :#line:7
+            print ("Buzz")#line:8
+        else :#line:9
+            print (OOO00OO0O00O00000 )#line:10
+if __name__ =="__main__":#line:12
+    main ()
