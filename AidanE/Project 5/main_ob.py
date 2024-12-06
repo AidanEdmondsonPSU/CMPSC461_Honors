b5fa482ce1192b9c82833c903e24d1a7dae19335
Ananya Gollakota <76816318+ananyag12@users.noreply.github.com>
2024-12-05 20:30:33 -0500

    Create main_ob.py

---
+++ AidanE/Project 5/main_ob.py
@@ -0,0 +1,9 @@
+def main ():#line:1
+    OO00O00O0OOOOOOOO =7 #line:2
+    O0000O00O000OO00O =int (input ("Guess a number between 1 and 10: "))#line:3
+    if O0000O00O000OO00O ==OO00O00O0OOOOOOOO :#line:4
+        print ("Correct!")#line:5
+    else :#line:6
+        print ("Try again.")#line:7
+if __name__ =="__main__":#line:9
+    main ()
