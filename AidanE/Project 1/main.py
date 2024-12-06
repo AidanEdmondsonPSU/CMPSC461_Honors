28566d366d8a0aa62fe35248b02b75d842b7b80a
AidanEdmondsonPSU <157417227+AidanEdmondsonPSU@users.noreply.github.com>
2024-12-05 20:20:52 -0500

    Update main.py

--- AidanE/Project 1/main.py
+++ AidanE/Project 1/main.py
@@ -1,6 +1,10 @@
-n = 10  # Number of terms in the Fibonacci sequence
-a, b = 0, 1
-print(a, b, end=" ")
-for _ in range(n - 2):
-    a, b = b, a + b
-    print(b, end=" ")
+def main():
+    n = 10  # Number of terms in the Fibonacci sequence
+    a, b = 0, 1
+    print(a, b, end=" ")
+    for _ in range(n - 2):
+        a, b = b, a + b
+        print(b, end=" ")
+
+if __name__ == "__main__":
+	main()
