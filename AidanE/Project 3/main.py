20330efa4987fafbdacf9e64293d2acc71afe69e
AidanEdmondsonPSU <157417227+AidanEdmondsonPSU@users.noreply.github.com>
2024-12-05 20:24:54 -0500

    Update main.py

--- AidanE/Project 3/main.py
+++ AidanE/Project 3/main.py
@@ -1,9 +1,13 @@
-for i in range(1, 16):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    else:
-        print(i)
+def main():
+    for i in range(1, 16):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+        
+if __name__ == "__main__":
+    main()
