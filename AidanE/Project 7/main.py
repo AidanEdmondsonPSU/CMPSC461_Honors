959db7a0469a5aada49452c492b4ac4d46be673a
Ananya Gollakota <76816318+ananyag12@users.noreply.github.com>
2024-12-05 20:28:33 -0500

    Update main.py

--- AidanE/Project 7/main.py
+++ AidanE/Project 7/main.py
@@ -1,5 +1,9 @@
-num = int(input("Enter a number: "))
-factorial = 1
-for i in range(1, num + 1):
-    factorial *= i
-print("Factorial:", factorial)
+def main():
+    num = int(input("Enter a number: "))
+    factorial = 1
+    for i in range(1, num + 1):
+        factorial *= i
+    print("Factorial:", factorial)
+
+if __name__ == "__main__":
+    main()
