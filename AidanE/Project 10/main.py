4c5cfc040e483835a4265650c804bdc880712234
Ananya Gollakota <76816318+ananyag12@users.noreply.github.com>
2024-12-05 20:23:35 -0500

    Update main.py

--- AidanE/Project 10/main.py
+++ AidanE/Project 10/main.py
@@ -1,5 +1,8 @@
-num = int(input("Enter a number: "))
-if num % 2 == 0:
-    print("Even")
-else:
-    print("Odd")
+def main ():#line:1
+    OO0OOOO000OOOO0OO =int (input ("Enter a number: "))#line:2
+    if OO0OOOO000OOOO0OO %2 ==0 :#line:3
+        print ("Even")#line:4
+    else :#line:5
+        print ("Odd")#line:6
+if __name__ =="__main__":#line:8
+    main ()
