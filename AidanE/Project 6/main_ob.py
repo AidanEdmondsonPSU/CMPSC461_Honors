b83376e7b03e8a37e29d09d804a0e09a9a8487ee
Ananya Gollakota <76816318+ananyag12@users.noreply.github.com>
2024-12-05 20:29:46 -0500

    Create main_ob.py

---
+++ AidanE/Project 6/main_ob.py
@@ -0,0 +1,6 @@
+def main ():#line:1
+    OOOOOOO00OO000000 =input ("Enter a string: ")#line:2
+    OOO00OO00OO00O000 =OOOOOOO00OO000000 [::-1 ]#line:3
+    print ("Reversed string:",OOO00OO00OO00O000 )#line:4
+if __name__ =="__main__":#line:6
+    main ()
