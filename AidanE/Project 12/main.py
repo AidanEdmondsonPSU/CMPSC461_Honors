5ad398ebd3afec6beb7c1f8b4609a9b92934c3ce
Ananya Gollakota <76816318+ananyag12@users.noreply.github.com>
2024-12-05 20:20:30 -0500

    Update main.py

--- AidanE/Project 12/main.py
+++ AidanE/Project 12/main.py
@@ -1,3 +1,6 @@
-num = int(input("Enter a number: "))
-for i in range(1, 11):
-    print(f"{num} x {i} = {num * i}")
+def main ():#line:1
+    O0O0O0OOOO0OOO0O0 =int (input ("Enter a number: "))#line:2
+    for O0OOOOO0OOO00OOOO in range (1 ,11 ):#line:3
+    	print (f"{O0O0O0OOOO0OOO0O0} x {O0OOOOO0OOO00OOOO} = {O0O0O0OOOO0OOO0O0 * O0OOOOO0OOO00OOOO}")#line:4
+if __name__ =="__main__":#line:6
+    main ()
