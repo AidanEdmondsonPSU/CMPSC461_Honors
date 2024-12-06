b1b25c47f65e5334cc3395d1fe9e2c5bd6700aea
Ananya Gollakota <76816318+ananyag12@users.noreply.github.com>
2024-12-05 20:22:51 -0500

    Update main.py

--- AidanE/Project 11/main.py
+++ AidanE/Project 11/main.py
@@ -1,7 +1,10 @@
-text = input("Enter a string: ")
-vowels = "aeiouAEIOU"
-count = 0
-for char in text:
-    if char in vowels:
-        count += 1
-print("Number of vowels:", count)
+def main ():#line:1
+    O0OO00000O00OOOO0 =input ("Enter a string: ")#line:2
+    O0OO00O00000OO000 ="aeiouAEIOU"#line:3
+    OOOOOO00OO000O0OO =0 #line:4
+    for OO00OOO000OOO0000 in O0OO00000O00OOOO0 :#line:5
+        if OO00OOO000OOO0000 in O0OO00O00000OO000 :#line:6
+            OOOOOO00OO000O0OO +=1 #line:7
+    print ("Number of vowels:",OOOOOO00OO000O0OO )#line:8
+if __name__ =="__main__":#line:10
+    main ()
