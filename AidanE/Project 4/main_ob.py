c960bfa437136f2047c8d72be2ad11bc5f0add6c
Ananya Gollakota <76816318+ananyag12@users.noreply.github.com>
2024-12-05 20:33:14 -0500

    Create main_ob.py

---
+++ AidanE/Project 4/main_ob.py
@@ -0,0 +1,6 @@
+def main ():#line:1
+    O0OOOOO000OOOOO00 =int (input ("Enter a number: "))#line:2
+    for O0000O0O0O0O000OO in range (1 ,11 ):#line:3
+        print (f"{O0OOOOO000OOOOO00} x {O0000O0O0O0O000OO} = {O0OOOOO000OOOOO00 * O0000O0O0O0O000OO}")#line:4
+if __name__ =="__main__":#line:6
+    main ()
