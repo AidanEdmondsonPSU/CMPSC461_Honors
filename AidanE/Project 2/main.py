f97c40b97236a7b6e18a46cc22413e3af2d9ed80
AidanEdmondsonPSU <157417227+AidanEdmondsonPSU@users.noreply.github.com>
2024-12-05 20:22:03 -0500

    Update main.py

--- AidanE/Project 2/main.py
+++ AidanE/Project 2/main.py
@@ -1,5 +1,9 @@
-numbers = [i for i in range(1, 6)]
-squares = [n * n for n in numbers]
+def main():
+  numbers = [i for i in range(1, 6)]
+  squares = [n * n for n in numbers]
+  
+  print("Numbers:", numbers)
+  print("Squares:", squares)
 
-print("Numbers:", numbers)
-print("Squares:", squares)
+if __name__ == "__main__":
+	main()
