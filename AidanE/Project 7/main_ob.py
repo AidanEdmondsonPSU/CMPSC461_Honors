3af0e45c1605af21d703087fd7ca07d3dfd1e518
Ananya Gollakota <76816318+ananyag12@users.noreply.github.com>
2024-12-05 20:28:54 -0500

    Create main_ob.py

---
+++ AidanE/Project 7/main_ob.py
@@ -0,0 +1,8 @@
+def main ():#line:1
+    O00000OOO0000OOO0 =int (input ("Enter a number: "))#line:2
+    OO000OOOO0O0O00O0 =1 #line:3
+    for O000000O00000O0OO in range (1 ,O00000OOO0000OOO0 +1 ):#line:4
+        OO000OOOO0O0O00O0 *=O000000O00000O0OO #line:5
+    print ("Factorial:",OO000OOOO0O0O00O0 )#line:6
+if __name__ =="__main__":#line:8
+    main ()
