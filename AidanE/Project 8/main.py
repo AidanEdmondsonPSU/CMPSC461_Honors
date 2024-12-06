b7065982d54973708adefee7841dca0d2a084579
Ananya Gollakota <76816318+ananyag12@users.noreply.github.com>
2024-12-05 20:25:02 -0500

    Update main.py

--- AidanE/Project 8/main.py
+++ AidanE/Project 8/main.py
@@ -1,5 +1,8 @@
-p = float(input("Principal amount: "))
-r = float(input("Rate of interest: "))
-t = float(input("Time in years: "))
-simple_interest = (p * r * t) / 100
-print("Simple Interest:", simple_interest)
+def main ():#line:1
+    O00OO00OOO0O0O0OO =float (input ("Principal amount: "))#line:2
+    O0OOO0O00000O00OO =float (input ("Rate of interest: "))#line:3
+    O000OOOO00OOOO00O =float (input ("Time in years: "))#line:4
+    OO0OO000000O0O000 =(O00OO00OOO0O0O0OO *O0OOO0O00000O00OO *O000OOOO00OOOO00O )/100 #line:5
+    print ("Simple Interest:",OO0OO000000O0O000 )#line:6
+if __name__ =="__main__":#line:8
+    main ()
